0e51331415de8479af6fcce686ef22a1b993b642
Mariusz Strohschein <mariuszkoniuchy@gmail.com>
2018-09-11 03:35:05 +0200

    Run_tests.sh runs coverege.

--- run_tests.sh
+++ run_tests.sh
@@ -1,24 +1,24 @@
-#/bin/bash
+#!/usr/bin/env bash
 PYENV_HOME=$WORKSPACE/.venv
 #if [ -d $PYENV_HOME ]; then
 #    rm -rf $PYENV_HOME
 #fi
 
 PATH=$WORKSPACE/.venv/bin:/usr/local/bin:$PATH
-if [ ! -d "venv" ]; then
+if [ ! -d ".venv" ]; then
 	virtualenv -p python3 $PYENV_HOME
 fi
-. venv/bin/activate
+. $PYENV_HOME/bin/activate
 
 echo "Virtualenv activated"
 pip --version
-pip install --quiet nosexcover
-pip install --quiet pylint
+pip install nosexcover
+#pip install pylint==2.1.1
 echo "Installed default requirements"
 # pip install --quiet $WORKSPACE/  # where your setup.py lives
-pip install $WORKSPACE/requirements.txt
+pip install -r $WORKSPACE/requirements.txt
 echo "Installed user requirements."
 export PYTHONPATH=$WORKSPACE
 
-nosetests --with-xcoverage --with-xunit --cover-package=freesm --cover-erase
-pylint -f parseable freesm/ | tee pylint.out
+nosetests --with-xcoverage --with-xunit --cover-package=. --cover-erase
+#pylint -f parseable freesm/ | tee pylint.out
