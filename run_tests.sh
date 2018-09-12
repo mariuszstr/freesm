fa88804f99ac142b88eb356dc6154ce1b9586e7a
Mariusz Strohschein <mariuszkoniuchy@gmail.com>
2018-09-12 04:20:45 +0200

    Fix env problem in run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -8,8 +8,8 @@ PYENV_HOME=$WORKSPACE/.venv
 if [ ! -d ".venv" ]; then
 	virtualenv -p python3 $PYENV_HOME
 fi
-source $PYENV_HOME/bin/activate
-. $PYENV_HOME/bin/activate
+#source $PYENV_HOME/bin/activate
+. ./$PYENV_HOME/bin/activate
 
 echo "Virtualenv activated"
 pip --version
