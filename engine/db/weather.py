98032206d484a23a2513d81d0d0392264cab9a78
Sylwester Lasota <mariuszkoniuchy@gmail.com>
2018-10-12 01:09:05 +0200

    delete heat class. WeatherType enum.

---
+++ engine/db/weather.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class WeatherType(Enum):
+    SUNNY = 1
+    SUNNY_WITH_LITTLE_CLOUDS = 2
+    CLOUDY = 2
+    LITTLE_RAIN = 3
+    RAIN = 4
+    HUGE_RAIN = 5
--- engine/heat/heat.py
+++ engine/heat/heat.py
@@ -1,16 +1,3 @@
 class Heat:
-    def __init__(self, heat_number, gateArider, gateBrider, gateCrider, gateDrider, trackSurface, trackLength):
-        #heat parameters
-        self.heat_number = heat_number
-        #lineup (from match program)
-        self.gateArider = gateArider
-        self.gateBrider = gateBrider
-        self.gateCrider = gateCrider
-        self.gateDrider = gateDrider
-        #track
-        self.trackSurface = trackSurface
-        self.trackLength = trackLength
-        '''Current weather conditions
-        self.currentTemperature = currentTemperature
-        self.currentWeathCond = currentWeathCond #rain, sunny, cloudy etc
-        '''
+    def __init__(self):
+        pass
