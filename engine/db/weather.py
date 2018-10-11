ad97abe19d83272b84c4acd96c6739d3efec02a2
Sylwester Lasota <mariuszkoniuchy@gmail.com>
2018-10-12 01:29:20 +0200

    generate_weather_type

--- engine/db/weather.py
+++ engine/db/weather.py
@@ -1,15 +1,22 @@
 from enum import Enum
+from random import random
 
 
 class WeatherType(Enum):
     SUNNY = 1
     SUNNY_WITH_LITTLE_CLOUDS = 2
-    CLOUDY = 2
-    LITTLE_RAIN = 3
-    RAIN = 4
-    HUGE_RAIN = 5
+    CLOUDY = 3
+    LITTLE_RAIN = 4
+    RAIN = 5
+    HUGE_RAIN = 6
 
 
 class Weather:
     def __init__(self, weather_type):
         self.weather_type = weather_type
+
+
+def generate_weather_type():
+    return random.choices([WeatherType.SUNNY] * 21 + [WeatherType.SUNNY_WITH_LITTLE_CLOUDS] * 60 +
+                          [WeatherType.CLOUDY] * 137 + [WeatherType.LITTLE_RAIN] * 63 + [WeatherType.RAIN] *
+                          60 + [WeatherType.HUGE_RAIN] * 23)
