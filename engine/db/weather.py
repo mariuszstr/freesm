568c23cea59d43da4e0dfa0e409bfcd96e528bf9
Sylwester Lasota <mariuszkoniuchy@gmail.com>
2018-10-12 10:50:46 +0200

    generate_temperature

--- engine/db/weather.py
+++ engine/db/weather.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from random import random
+from random import random, randint
 
 
 class WeatherType(Enum):
@@ -20,3 +20,12 @@ def generate_weather_type():
     return random.choices([WeatherType.SUNNY] * 21 + [WeatherType.SUNNY_WITH_LITTLE_CLOUDS] * 60 +
                           [WeatherType.CLOUDY] * 137 + [WeatherType.LITTLE_RAIN] * 63 + [WeatherType.RAIN] *
                           60 + [WeatherType.HUGE_RAIN] * 23)
+
+
+TEMPERATURES_RANGES = (
+    (-25,  4), (-21,  6), (-5, 15), (3, 22), (8, 25), (10, 33), (13, 40), (13, 40), (9, 33), (5, 21), (2, 14), (-10, 5)
+)
+
+
+def generate_temperature(month):
+    return randint(TEMPERATURES_RANGES[month - 1], TEMPERATURES_RANGES[month - 1])
