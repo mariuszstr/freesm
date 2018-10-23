13c88e95f65e03da4348ca7d3a9b6b2efd67df62
Sylwester Lasota <mariuszkoniuchy@gmail.com>
2018-10-24 00:45:40 +0200

    LeagueClass docstrings.

--- engine/db/league_class.py
+++ engine/db/league_class.py
@@ -1,5 +1,17 @@
 class LeagueClass:
+    """League class - for example "PGE Ekstraliga".
+
+    """
     def __init__(self, league, name, ability, next_class, previous_class):
+        """Constructor for league class object.
+
+        Args:
+            league (League): country league for league class.
+            name (String): League class name (for example, "PGE Ekstraliga")
+            ability (Int): League ability.
+            next_class (LeagueClass): LeagueClass object for promotion teams.
+            previous_class(LeagueClass): LeagueClass object for degradation.
+        """
         self.league = league
         self.name = name
         self.ability = ability
@@ -7,4 +19,13 @@ class LeagueClass:
         self.previous_class = previous_class
 
     def __eq__(self, other):
+        """Check if two LeagueClass objects are equals.
+
+        Args:
+            other (LeagueClass): other league class to compare.
+
+        Returns:
+            bool: The return value - if league classes are equal.
+
+        """
         return self.name == other.name
