0442899bde4cda43b630ab27204b5e071ca9d5f5
Sylwester Lasota <mariuszkoniuchy@gmail.com>
2018-10-24 00:20:20 +0200

    Docstrings for City class.

--- engine/db/city.py
+++ engine/db/city.py
@@ -1,5 +1,14 @@
 class City:
     def __init__(self, name, country, population, wealth, speedway_popularity):
+        """Constructor for City class.
+
+        Args:
+            name (String): City name.
+            country (Country): Country object.
+            population (Int): City population.
+            wealth( Int): Wealth of the city.
+            speedway_popularity (Int): Speedway popularity in the city.
+        """
         self.name = name
         self.country = country
         self.population = population
@@ -7,4 +16,13 @@ class City:
         self.speedway_popularity = speedway_popularity
 
     def __eq__(self, other):
+        """Compare two cieties.
+
+        Args:
+            other (City): City to compare with.
+
+        Returns:
+            bool: The return value - if cities are equal.
+
+        """
         return self.name == other.name
