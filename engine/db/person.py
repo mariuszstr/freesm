d92aa945c14843c8a1fbeefa4cd1f91589b22a8e
Sylwester Lasota <mariuszkoniuchy@gmail.com>
2018-10-24 01:03:15 +0200

    Person docstrings.

--- engine/db/person.py
+++ engine/db/person.py
@@ -1,6 +1,26 @@
 class Person:
+    """Person.
+
+    """
     def __init__(self, first_name, last_name, rider, birth_date, determination, loyalty,
                  weight, height, iq, bravery, strength, condition, reflex):
+        """Person's constructor.
+
+        Args:
+            first_name (String): First name.
+            last_name (String): Last name.
+            rider (Rider): Skills for ride.
+            birth_date (Date): Birth's date.
+            determination (Int): Determination (1-100).
+            loyalty (Int): Loyalt (1-100)
+            weight (Int): Weight.
+            height (Int): Height
+            iq (Int): Iq.
+            bravery (Int): Bravery (1-100)
+            strength (Int): Strength (1-100)
+            condition (Int): Condition (1-100)
+            reflex (Int): Reflex (1-100)
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.rider = rider
@@ -16,6 +36,15 @@ class Person:
         self.reflex = reflex
 
     def __eq__(self, other):
-        return self.first_name == other.first_name and self.last_name == other.last_name and \
-               self.birth_date.day == other.birth_date.day and self.birth_date.month == other.birth_date.month and \
+        """Equals two persons.
+
+        Args:
+            other (Person): other Person object.
+
+        Returns:
+            bool: The return value - if persons are equal.
+
+        """
+        return self.first_name == other.first_name and self.last_name == other.last_name\
+               and self.birth_date.day == other.birth_date.day and self.birth_date.month == other.birth_date.month and \
                self.birth_date.year == other.birth_date.year
